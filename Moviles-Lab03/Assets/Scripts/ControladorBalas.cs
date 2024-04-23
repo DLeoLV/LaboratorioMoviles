b99ff4f775ffbdc77c37803cf9dcd3a5a11ad6e6
DLeoLV <131694293+DLeoLV@users.noreply.github.com>
2024-04-23 10:37:36 -0500

    Cambios2

--- Moviles-Lab03/Assets/Scripts/ControladorBalas.cs
+++ Moviles-Lab03/Assets/Scripts/ControladorBalas.cs
@@ -4,37 +4,34 @@ using UnityEngine;
 
 public class ControladorBalas : MonoBehaviour
 {
-    public GameObject balaPrefab; // Prefab de la bala a disparar
-    public float tiempoParaDisparar = 1f; // Tiempo necesario para disparar después de sostener presionado (en segundos)
+    public GameObject balaPrefab; 
+    public float tiempoParaDisparar = 1f; 
 
-    private bool estaPresionado = false; // Indica si el botón está siendo presionado
-    private bool dispararHabilitado = false; // Indica si se puede disparar
-    private float tiempoPresionado = 0f; // Tiempo que se ha mantenido presionado
+    private bool estaPresionado = false; 
+    private bool dispararHabilitado = false; 
+    private float tiempoPresionado = 0f;
 
     void Update()
     {
-        // Verificar si se presiona el botón
         if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
         {
             estaPresionado = true;
-            tiempoPresionado = 0f; // Reiniciar el tiempo presionado
+            tiempoPresionado = 0f;
         }
-        // Verificar si se mantiene presionado el botón
+
         else if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Stationary)
         {
-            tiempoPresionado += Time.deltaTime; // Incrementar el tiempo presionado
-            // Si el tiempo presionado es mayor que el tiempo para disparar, habilitar el disparo
+            tiempoPresionado += Time.deltaTime;
             if (tiempoPresionado >= tiempoParaDisparar && !dispararHabilitado)
             {
                 dispararHabilitado = true;
-                InvokeRepeating("Disparar", 0f, 1f); // Invocar el método Disparar cada segundo
+                InvokeRepeating("Disparar", 0f, 1f);
             }
         }
-        // Verificar si se levanta el botón
+
         else if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended)
         {
             estaPresionado = false;
-            // Si se estaba manteniendo presionado y ahora se levantó, cancelar el disparo
             if (dispararHabilitado)
             {
                 dispararHabilitado = false;
@@ -42,8 +39,6 @@ public class ControladorBalas : MonoBehaviour
             }
         }
     }
-
-    // Método para disparar una bala
     void Disparar()
     {
         Instantiate(balaPrefab, transform.position, Quaternion.identity);
